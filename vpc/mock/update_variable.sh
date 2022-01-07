d6cb5dbae999904152872a4a4913561c6290cef4
Andrew Way <andrew.way@env0.com>
2022-01-07 08:04:40 -0800

    fixed script

--- vpc/mock/update_variable.sh
+++ vpc/mock/update_variable.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -x
+
 curl --request POST \
 --url https://api.env0.com/configuration \
 --user $ENV0_API_KEY:$ENV0_API_SECRET \
 --header "Content-Type: application/json" \
 --data "[{
   \"name\": \"vpc_id\",
-  \"value\": \"$(terraform output vpc_id)\",
+  \"value\": $(terraform output vpc_id),
   \"isReadonly\": true,
   \"isRequired\": true,
   \"scopeId\": \"$ENV0_PROJECT_ID\",
