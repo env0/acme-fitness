9d012530c079c6551c1fdea2007626b5eaea93e3
Andrew Way <andrew.way@env0.com>
2022-02-11 08:49:11 -0800

    aws frontend example with module

---
+++ frontend/aws/main.tf
@@ -0,0 +1,13 @@
+resource "random_string" "random" {
+  length  = 5
+  special = false
+  lower   = true
+  upper   = false
+  number  = true
+  keepers = {"index.html":"${filemd5("index.html")}"}
+}
+
+module "s3" {
+  source  = "../../modules/s3"
+  bucketname = "acme-frontend-${random_string.random.id}"
+}
---
+++ frontend/aws/outputs.tf
@@ -0,0 +1,3 @@
+output "url" {
+  value = "http://${module.s3.url}"
+}
