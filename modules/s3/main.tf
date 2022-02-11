e7de10d0144a9e0c2df31b25fe2f0364b5a80d3e
Andrew Way <andrew.way@env0.com>
2022-02-11 08:56:02 -0800

    fix module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -13,7 +13,7 @@ module "s3" {
       "Effect":"Allow",
       "Principal": "*",
       "Action":["s3:GetObject","s3:GetObjectVersion"],
-      "Resource":["${module.acme-s3.s3_bucket_arn}"]
+      "Resource":["${module.s3.s3_bucket_arn}"]
     }
   ]
   EOT
