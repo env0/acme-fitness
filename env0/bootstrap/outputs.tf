3d01e8e3b4c70ae0f30c391c52e36bbdde96e6ac
Andrew Way <andrew.way@env0.com>
2024-05-29 22:20:49 -0700

    fix outputs

--- env0/bootstrap/outputs.tf
+++ env0/bootstrap/outputs.tf
@@ -1,9 +1,17 @@
 output "deployer_arn" {
-  value       = module.assume-role.env0_deployer_role.arn
+  value       = module.assume-role.deployer_arn
   description = "deployer role"
 }
 
 output "cost_role_arn" {
-  value       = module.assume-role.env0_cost_role.arn
+  value       = module.assume-role.cost_role_arn
   description = "cost role"
+}
+
+output "ssh_public_key" {
+  value = module.github-sshkey.env0_ssh_public_key
+}
+output "ssh_private_key" {
+  value     = module.github-sshkey.env0_ssh_private_key
+  sensitive = true
 }
