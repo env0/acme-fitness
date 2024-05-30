0cd98b9866074adec886d81c4de7309e2ec5dc8e
Andrew Way <andrew.way@env0.com>
2024-05-29 21:47:28 -0700

    fix variable

--- env0/credentials/ssh/outputs.tf
+++ env0/credentials/ssh/outputs.tf
@@ -1,8 +1,8 @@
 output "env0_ssh_public_key" {
-  value = tls_private_key.github_key.public_key_openssh
+  value = tls_private_key.this.public_key_openssh
 }
 
 output "env0_ssh_private_key" {
-  value     = tls_private_key.github_key.private_key_pem
+  value     = tls_private_key.this.private_key_pem
   sensitive = true
 }
