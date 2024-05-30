8091b238e1ab6d67893ee0a4c1addaef11f53218
Andrew Way <andrew.way@env0.com>
2024-05-29 22:48:06 -0700

    add s3 and website

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -28,3 +28,23 @@ resource "aws_s3_bucket_acl" "this" {
   bucket = aws_s3_bucket.this.id
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.this.bucket
+  key    = "index.html"
+  source = "index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("index.html")
+}
+
