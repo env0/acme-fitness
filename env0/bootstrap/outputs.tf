9de13408b3a943b68bb7141fe74e0d7a584f326d
Andrew Way <andrew.way@env0.com>
2024-06-21 15:21:20 -0700

    remote github keys

--- env0/bootstrap/outputs.tf
+++ env0/bootstrap/outputs.tf
@@ -7,11 +7,3 @@ output "cost_role_arn" {
   value       = module.assume-role.cost_role_arn
   description = "cost role"
 }
-
-output "ssh_public_key" {
-  value = module.github-sshkey.env0_ssh_public_key
-}
-output "ssh_private_key" {
-  value     = module.github-sshkey.env0_ssh_private_key
-  sensitive = true
-}
