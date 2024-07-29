d43a106883015dc3875f5cd358afd6ebac6de408
Andrew Way <andrew.way@env0.com>
2024-07-29 08:18:11 -0700

    add module

---
+++ env0/bootstrap/env0_modules.tf
@@ -0,0 +1,18 @@
+resource "env0_module" "vpc" {
+  module_name         = "vpc"
+  module_provider     = "aws"
+  repository          = data.env0_template.this.repository
+  path                = "modules/vpc"
+  tag_prefix          = "vpc"
+  module_test_enabled = true
+  opentofu_version    = "latest"
+
+  # vcs
+# vcs configuration
+  github_installation_id = var.vcs == "github" ? data.env0_template.this.github_installation_id : null
+  bitbucket_client_key   = var.vcs == "bitbucket" ? data.env0_template.this.bitbucket_client_key : null
+  # gitlab_project_id    = data.env0_template.this.gitlab_project_id
+  is_azure_devops = var.vcs == "azure" ? data.env0_template.this.is_azure_devops : null
+  token_id        = var.vcs == "gitlab" || var.vcs == "azure" ? data.env0_template.this.token_id : null
+  #token_name      = var.vcs == "gitlab" ? data.env0_template.this.token_name : null
+}
