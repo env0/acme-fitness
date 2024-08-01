a08c81bb3adef4f84275d077d1da82291797c769
Andrew Way <andrew.way@env0.com>
2024-08-01 14:18:53 -0700

    remove env0_environment from projects

--- env0/bootstrap/env0_projects.tf
+++ env0/bootstrap/env0_projects.tf
@@ -102,36 +102,3 @@ resource "env0_configuration_variable" "environment_policies" {
   }
   EOT
 }
-
-resource "env0_environment" "default_projects" {
-  count = var.create_projects ? 1 : 0
-
-  name                       = "${var.default_team_name} Project"
-  project_id                 = data.env0_environment.this.project_id
-  template_id                = env0_template.projects.id
-  approve_plan_automatically = true
-  is_remote_backend          = true
-  removal_strategy           = "mark_as_archived"
-  workspace                  = "${var.default_team_name}_project"
-
-  configuration {
-    name        = "team_name"
-    value       = var.default_team_name
-    type        = "terraform"
-    description = "main project name for team"
-    regex       = "[a-zA-Z0-9-_]*"
-    is_required = true
-  }
-
-  depends_on = [env0_configuration_variable.team_name,
-    env0_configuration_variable.team_environments,
-    env0_configuration_variable.environment_policies,
-    env0_template_project_assignment.projects,
-  env0_api_key.mgmt]
-
-  lifecycle {
-    ignore_changes = [
-      variable_sets
-    ]
-  }
-}
